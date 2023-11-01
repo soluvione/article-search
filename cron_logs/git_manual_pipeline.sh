542cf2d9f08a3a0dd3691e1387fe4ae436b9eae2
Ubuntu <ubuntu@ip-172-31-44-244.eu-north-1.compute.internal>
2023-11-01 17:50:05 +0300

    Logs Update Commit

--- cron_logs/git_manual_pipeline.sh
+++ cron_logs/git_manual_pipeline.sh
@@ -16,3 +16,29 @@ After doing this, you may fix the identity used for this commit with:
  create mode 100644 cron_logs/git_manual_pipeline.sh
  create mode 100644 cron_logs/unq_tk.log
 Already up to date.
+[vm-test-branch 3febdf5] Weekly Project Update for Week 43-2023
+ Committer: Ubuntu <ubuntu@ip-172-31-44-244.eu-north-1.compute.internal>
+Your name and email address were configured automatically based
+on your username and hostname. Please check that they are accurate.
+You can suppress this message by setting them explicitly:
+
+    git config --global user.name "Your Name"
+    git config --global user.email you@example.com
+
+After doing this, you may fix the identity used for this commit with:
+
+    git commit --amend --reset-author
+
+ 84 files changed, 996 insertions(+), 83 deletions(-)
+ create mode 100644 cron_logs/col_m9.log
+ create mode 100644 cron_logs/col_md12.log
+ create mode 100644 cron_logs/dergi_platformu.log
+ create mode 100644 cron_logs/dergipark_no_ref.log
+ create mode 100644 cron_logs/firat.log
+ create mode 100644 cron_logs/karep.log
+ create mode 100644 cron_logs/pkp.log
+ create mode 100644 cron_logs/sayi_sayfalar.log
+ create mode 100644 cron_logs/span9.log
+ create mode 100644 cron_logs/tk_no_ref_1-97.log
+ create mode 100644 cron_logs/tubitak.log
+ create mode 100644 downloads_n_logs/firat_manual/sunday_1-5_firat/1_firattpdergisi/downloads/pdf_FTD_1369.pdf
